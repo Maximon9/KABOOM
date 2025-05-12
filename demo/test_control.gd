f40a7b43e8f6101e1973281a0619f122ba9a966f
Maximon9 <aamaximon989@gmail.com>
2025-05-12 05:31:08 -0500

    Updated: KABOOM.windows.template_debug.x86_64.dll, test_control.gd, kaboom_canvas_item.hpp;

--- demo/test_control.gd
+++ demo/test_control.gd
@@ -2,9 +2,9 @@
 extends Control
 
 func _ready() -> void:
-    # var test_child: KABOOMControl = KABOOMControl.new()
-    # test_child.name = "Test"
-    # add_child(test_child)
-    # if Engine.is_editor_hint():
-    #     test_child.owner = get_tree().edited_scene_root
-    pass
+    var test_child: KABOOMControl = KABOOMControl.new()
+    test_child.name = "Test"
+    add_child(test_child)
+    if Engine.is_editor_hint():
+        test_child.owner = get_tree().edited_scene_root
+    test_child.test()
